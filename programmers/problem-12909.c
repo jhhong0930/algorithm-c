86fe9d7a62ff0ebb519ec531e64c2d45c41afd78
jhhong0930 <jhhong0930@aift.kr>
2023-09-16 19:13:55 +0900

    올바른 괄호 축약 버전으로 다시 풀이

--- programmers/problem-12909.c
+++ programmers/problem-12909.c
@@ -4,44 +4,22 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#define MAX_STACK_SIZE 100000
 
-char stack[MAX_STACK_SIZE];
-int top = -1;
-
-bool isEmpty() {
-    return top < 0;
-}
-
-void push(char value) {
-    stack[++top] = value;
-}
-
-char pop() {
-    if (isEmpty()) {
-        printf("Stack is empty!");
-        return 0;
-    } else {
-        return stack[top--];
-    }
-}
-
+// 스택에 넣는다고 가정만 하고 top 의 위치로만 판단
 bool solution(const char* s) {
-    for (int i=0; s[i] != '\0'; i++) {
-        char current = s[i];
-        if (current == '(') {
-            push(current);
+    int top = -1;
+    
+    for (int i=0; s[i]!='\0'; i++) {
+        if (s[i] == '(') {
+            top++;
         } else {
-            if (isEmpty()) return false;
-            pop();
+            if (top < 0) return false;
+            top--;
         }
     }
-    return isEmpty();
+    
+    return top < 0;
 }
-
-// stack 이 비었는데 닫는 괄호가 나오는 경우 -> false
-// for 문을 전부 돌았는데 stack 이 비어있지 않은경우 -> false
-// for 문을 전부 돌았고 stack 도 비어있는 경우 -> true
 
 int main(void) {
     bool result = solution("()()");
